e3b1d13c6c167b6ace14bccc47475b4e3a286653
Sebastián Marciales <Sebastianmarciales40@gmail.com>
2025-05-09 20:07:10 -0500

    Actualizo URL del backend en producción a Railway

--- todo-list-app/src/environments/environment.prod.ts
+++ todo-list-app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tu-backend-url.com/api/todos' // Aquí deberás reemplazar con la URL de tu backend en producción
+  apiUrl: 'https://todolist-production-edf8.up.railway.app/api/todos'
 }; 
